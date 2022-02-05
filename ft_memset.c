4d8eb141c06c3ae9f3e7f0c09431b718486370ee
Mustafa Akcanca <makcanca@k1m38s03.42istanbul.com.tr>
2022-02-05 20:12:25 +0300

    Fix for memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,9 +6,11 @@
 /*   By: makcanca <mustafa.akcanca@takyonai.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 20:06:17 by makcanca          #+#    #+#             */
-/*   Updated: 2022/02/05 20:08:56 by makcanca         ###   ########.fr       */
+/*   Updated: 2022/02/05 20:12:05 by makcanca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
 void	*ft_memset(void *str, int c, size_t n)
 {
 	size_t	i;
@@ -16,7 +18,10 @@ void	*ft_memset(void *str, int c, size_t n)
 
 	i = 0;
 	buffer = str;
-	while (i++ < n)
+	while (i < n)
+	{
 		buffer[i] = c;
+		i++;
+	}
 	return (buffer);
 }
