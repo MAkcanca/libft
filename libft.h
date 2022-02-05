f4482ba7b7c159cc5a435a03085ff028b6f8d280
Mustafa Akcanca <makcanca@k1m38s03.42istanbul.com.tr>
2022-02-05 20:09:36 +0300

    Various fixes for norminette

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: makcanca <mustafa.akcanca@takyonai.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 17:48:51 by makcanca          #+#    #+#             */
-/*   Updated: 2022/02/05 19:50:32 by makcanca         ###   ########.fr       */
+/*   Updated: 2022/02/05 20:09:24 by makcanca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 # define LIBFT_H
 # include <string.h>
 
-int	ft_toupper(int c);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *c);
-
+void	*ft_memset(void *str, int c, size_t n);
 #endif
