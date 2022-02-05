51afc562707a94b2fbb1a86a8c514ed7cd430f5e
Mustafa Akcanca <makcanca@k1m38s03.42istanbul.com.tr>
2022-02-05 22:10:59 +0300

    Add various function prototypes to libft.h

--- libft.h
+++ libft.h
@@ -6,13 +6,15 @@
 /*   By: makcanca <mustafa.akcanca@takyonai.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 17:48:51 by makcanca          #+#    #+#             */
-/*   Updated: 2022/02/05 20:37:42 by makcanca         ###   ########.fr       */
+/*   Updated: 2022/02/05 21:55:13 by makcanca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 int		ft_toupper(int c);
 int		ft_isalpha(int c);
@@ -26,4 +28,8 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(char const *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+void	*ft_calloc(size_t nbr, size_t size);
 #endif
